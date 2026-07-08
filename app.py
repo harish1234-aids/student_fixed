cd277bf563b025332022482704c8039a28cd8fae
harish1234-aids <ragavharish@gmail.com>
2026-07-08 10:43:16 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient("mongodb+srv://ragavharish749_db_user:TdmMgQrqTLqUQKX6@student-db.xzn9oa4.mongodb.net/?appName=student-db")
+
+db = client["student_db"]
+students = db["students"]
+
+@app.route("/")
+def home():
+    data = students.find()
+    return render_template("index.html", students=data)
+
+@app.route("/add", methods=["POST"])
+def add():
+    name = request.form["name"]
+
+    students.insert_one({
+        "name": name
+    })
+
+    return redirect("/")
+
+@app.route("/delete/<name>")
+def delete(name):
+    students.delete_one({"name": name})
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
